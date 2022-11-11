cd89fd5e97ed4911105c1023d7df299f60e16982
sepezho <sepezho@gmail.com>
2022-11-11 21:15:52 +0100

    fix prices

--- src/logic/fetch/fetchMarkets.ts
+++ src/logic/fetch/fetchMarkets.ts
@@ -1,38 +1,19 @@
 import CoinGecko from "coingecko-api";
 
-
-const fetchMarkets = (stu: any, ssu: any, sau: any, snu:any,sauruu:any, susn: any) => {
-	fetch("https://proxy.tonana.org/https://ftx.com/api/markets")
-		.then((e) => e.json())
-		.then(async (e) => {
-			stu(e.result.filter((item: any) => item.name === "TONCOIN/USD")[0].price);
-			ssu(e.result.filter((item: any) => item.name === "SOL/USD")[0].price);
-			sau(e.result.filter((item: any) => item.name === "ATOM/USD")[0].price);
-			snu(e.result.filter((item: any) => item.name === "NEAR/USD")[0].price);
-			await func(sauruu)
-			await funac(susn)
-			
-		});
+const fetchMarkets = async (stu: any, ssu: any, sau: any, snu:any,sauruu:any, susn: any) => {
+	const CoinGeckoClient = new CoinGecko();
+	stu((await CoinGeckoClient.coins.fetch("the-open-network", {})).data
+	.market_data.current_price.usd);
+	ssu((await CoinGeckoClient.coins.fetch("solana", {})).data
+	.market_data.current_price.usd);
+	sau((await CoinGeckoClient.coins.fetch("cosmos", {})).data
+	.market_data.current_price.usd);
+	snu((await CoinGeckoClient.coins.fetch("near", {})).data.market_data
+	.current_price.usd);
+	sauruu((await CoinGeckoClient.coins.fetch("aurora-near", {})).data
+	.market_data.current_price.usd)
+	susn((await CoinGeckoClient.coins.fetch("usn", {})).data.market_data
+	.current_price.usd)
 };
-
-
-
-//2. Initiate the CoinGecko API Client
-const CoinGeckoClient = new CoinGecko();
-
-//3. Make calls
-var func = async (sauruu:any) => {
-	// console.log( await CoinGeckoClient.coins.all());
-
-	let data = await CoinGeckoClient.coins.fetch("aurora-near", {});
-	sauruu(data.data.market_data.current_price.usd);
-};
-var funac = async (sauruu:any) => {
-	let data = await CoinGeckoClient.coins.fetch("usn", {});
-	console.log(data);
-	sauruu(data.data.market_data.current_price.usd);
-};
-
-
 
 export default fetchMarkets;
