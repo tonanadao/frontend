2acf560edddf8af942522336e5ff7b36c8ed3c65
Dmitry Kodolbenko <115988813+DmitryKodolbenko@users.noreply.github.com>
2023-04-23 03:00:07 +0300

    menu builder done

--- src/components/MenuBuilder.tsx
+++ src/components/MenuBuilder.tsx
@@ -1,64 +1,56 @@
 import { Menu } from "antd";
 import arrOfChains from "../config"
+import { retry } from "rxjs";
 
 // net - opposite network
 // set - net setter
 // formType - type of shape (bridge or swap)
 // isDestination - an indicator of where we are (dest or source)
+
+
+// For the swap form, we return to the menu all blockchains from the config,
+// except for the wrappers and the blockchain that is currently set in the opposite menu. 
+// One condition is valid for both "destination" and "source".
+
+// When we are in bridge form and in "source",
+// we return all the contents of the config except TON.
+// When we are in bridge form in "destination", we only return the opposite chain's wrapper.
+
+
+
 export const menuBuilder = (net: string, set: any, formType: string, isDestination: boolean) => (
 	<Menu
-		 items =  
-		{[ arrOfChains.map((item: any) => {
-
-			let returnableItem: string = item;
-
-			if (formType === 'swap' && item !== net && !item.includes('(') && !item.includes(')')) {
-				returnableItem = item;
-			} 
+		 items = { arrOfChains.map((item: string) => {
+		
+			if (formType === 'swap' && item !== net && !item.includes('(') && !item.includes(')') ||
+			    formType === 'bridge' && !isDestination && item !== 'TON')  {
+				return (
+					{
+						key: item,
+						label: (
+							<div
+								className={
+									(formType === 'swap' && net !== `w${item} (TON)`) ||
+										(formType === 'bridge' && !isDestination)
+										? ""
+										: "cantSelect"
+								}
+								onClick={() =>
+									(formType === 'swap' && net !== `w${item} (TON)`) ||
+										(formType === 'bridge' && !isDestination)
+										? set(item)
+										: null
+								}>
+								{item}
+							</div>
+						),
+					}
+					
+				)
+			} else {return null}
 			
-			else if (formType === 'bridge' && !isDestination && item !== 'TON') {
-				returnableItem = item;
-			} 
-			
-			else if (formType === 'bridge' && isDestination) {
-
-				// for wraper opposite destination
-				if (net.includes('(') && net.includes(')') && net.includes(item) && !item.includes('(') && !item.includes(')')) {
-					returnableItem = item;
-				}
-				
-				// for clear net opposite destination
-				 else if (item.includes(net)) {returnableItem = item;}
-
-			}
-
-
-
-			return (
-				{
-					key: returnableItem,
-					label: (
-						<div
-							className={
-								(formType === 'swap' && net !== `w${returnableItem} (TON)`) ||
-									(formType === 'bridge' && !isDestination)
-									? ""
-									: "cantSelect"
-							}
-							onClick={() =>
-								(formType === 'swap' && net !== `w${returnableItem} (TON)`) ||
-									(formType === 'bridge' && !isDestination)
-									? set(returnableItem)
-									: null
-							}>
-							{returnableItem}
-						</div>
-					),
-				}
-				
-			);
 		})
 
-		]}
+		}
 	/>
 );
