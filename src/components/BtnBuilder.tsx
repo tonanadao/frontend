333c3c0d58548635f050b47bb5d666b692129bad
sepezho <47150504+sepezho@users.noreply.github.com>
2023-04-25 22:19:59 +0100

    fixes

--- src/components/BtnBuilder.tsx
+++ src/components/BtnBuilder.tsx
@@ -1,35 +1,13 @@
-
-
 import { Button } from "antd";
-
-import React, { Fragment, useCallback, useEffect, useState } from "react";
-import { providers, utils } from "near-api-js";
-import type {
-	AccountView,
-	CodeResult,
-} from "near-api-js/lib/providers/provider";
-import type { Transaction } from "@near-wallet-selector/core";
-
-import { useWalletSelector } from "../contexts/WalletSelectorContext";
-
-type Submitted = SubmitEvent & {
-	target: { elements: { [key: string]: HTMLInputElement } };
-};
-
 import phantom from "../static/img/phantom.png";
 import near from "../static/img/near.png";
 import tonIco from "../static/img/ton.png";
 import keplr from "../static/img/keplr.png";
 import metamask from "../static/img/metamask.png";
 
-
-const SUGGESTED_DONATION = "0";
-const BOATLOAD_OF_GAS = utils.format.parseNearAmount("0.00000000003")!;
-
 const zipName = (name: string) => `${name.slice(0, 5)}...${name.slice(-3)}`;
 
 export const generateBtn = (props: any, currencyName: string) => {
-
 	const { connectWalletSOL,
 		connectWalletETH,
 		setSOLWalletKey,
@@ -48,10 +26,7 @@ export const generateBtn = (props: any, currencyName: string) => {
 		NEARwalletKey,
 		ATOMwalletKey,
 		ETHwalletKey,
-	    selector,
-		modal, 
-		accounts, 
-		accountId } = props;
+	} = props;
 
 	const sortedBtnProps: any = {
 		sol: {
@@ -70,7 +45,7 @@ export const generateBtn = (props: any, currencyName: string) => {
 			connect: connectWalletATOM,
 			set: setATOMwalletKey,
 			walletKey: ATOMwalletKey,
-			img: keplr, 
+			img: keplr,
 		},
 		aurora: {
 			connect: connectWalletAUR,
@@ -94,23 +69,23 @@ export const generateBtn = (props: any, currencyName: string) => {
 
 	let key: string = currencyName.toLocaleLowerCase();
 	if (key.includes('ton')) { key = "ton" }
-	if (key.includes('usn')) {key = "near"}
+	if (key.includes('usn')) { key = "near" }
 
 	return (
 		<>
-				<Button
-					type="primary"
-					id={"connectWalletBtn"}
-					onClick={() => sortedBtnProps[key].connect(sortedBtnProps[key].set)}>
-					{sortedBtnProps[key].walletKey ? (
-						<>
-							<img src={sortedBtnProps[key].img} alt={"Wallet picture"} />
-							{zipName(sortedBtnProps[key].walletKey)}
-						</>
-					) : (
-						"Connect wallet"
-					)}
-				</Button>
+			<Button
+				type="primary"
+				id={"connectWalletBtn"}
+				onClick={() => sortedBtnProps[key].connect(sortedBtnProps[key].set)}>
+				{sortedBtnProps[key].walletKey ? (
+					<>
+						<img src={sortedBtnProps[key].img} alt={"Wallet picture"} />
+						{zipName(sortedBtnProps[key].walletKey)}
+					</>
+				) : (
+					"Connect wallet"
+				)}
+			</Button>
 		</>
 	);
 };
--- src/components/MenuBuilder.tsx
+++ src/components/MenuBuilder.tsx
@@ -1,29 +1,11 @@
 import { Menu } from "antd";
 import arrOfChains from "../config"
-import { retry } from "rxjs";
-
-// net - opposite network
-// set - net setter
-// formType - type of shape (bridge or swap)
-// isDestination - an indicator of where we are (dest or source)
-
-
-// For the swap form, we return to the menu all blockchains from the config,
-// except for the wrappers and the blockchain that is currently set in the opposite menu. 
-// One condition is valid for both "destination" and "source".
-
-// When we are in bridge form and in "source",
-// we return all the contents of the config except TON.
-// When we are in bridge form in "destination", we only return the opposite chain's wrapper.
-
-
 
 export const menuBuilder = (net: string, set: any, formType: string, isDestination: boolean) => (
 	<Menu
-		 items = { arrOfChains.map((item: string) => {
-		
+		items={arrOfChains.map((item: string) => {
 			if (formType === 'swap' && item !== net && !item.includes('(') && !item.includes(')') ||
-			    formType === 'bridge' && !isDestination && item !== 'TON')  {
+				formType === 'bridge' && !isDestination && item !== 'TON') {
 				return (
 					{
 						key: item,
@@ -45,12 +27,10 @@ export const menuBuilder = (net: string, set: any, formType: string, isDestinati
 							</div>
 						),
 					}
-					
-				)
-			} else {return null}
-			
-		})
 
+				)
+			} else { return null }
+		})
 		}
 	/>
 );
